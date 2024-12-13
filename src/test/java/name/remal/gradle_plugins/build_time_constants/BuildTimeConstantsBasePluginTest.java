ae9bd8b0d1e99977fb32f9e104588389cf7e7de4
Semyon Levin <levin.semen@gmail.com>
2024-12-12 16:42:05 -0800

    [release-later] Additional tests

--- src/test/java/name/remal/gradle_plugins/build_time_constants/BuildTimeConstantsBasePluginTest.java
+++ src/test/java/name/remal/gradle_plugins/build_time_constants/BuildTimeConstantsBasePluginTest.java
@@ -1,8 +1,13 @@
 package name.remal.gradle_plugins.build_time_constants;
 
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.packageNameOf;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.unwrapGeneratedSubclass;
+import static name.remal.gradle_plugins.toolkit.testkit.ProjectValidations.executeAfterEvaluateActions;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import name.remal.gradle_plugins.toolkit.testkit.TaskValidations;
 import org.gradle.api.Project;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +25,19 @@ class BuildTimeConstantsBasePluginTest {
     @Test
     void extensionIsAdded() {
         assertNotNull(project.getExtensions().getByType(BuildTimeConstantsExtension.class));
+    }
+
+    @Test
+    void pluginTasksDoNotHavePropertyProblems() {
+        executeAfterEvaluateActions(project);
+
+        val taskClassNamePrefix = packageNameOf(BuildTimeConstantsBasePlugin.class) + '.';
+        project.getTasks().stream()
+            .filter(task -> {
+                val taskClass = unwrapGeneratedSubclass(task.getClass());
+                return taskClass.getName().startsWith(taskClassNamePrefix);
+            })
+            .forEach(TaskValidations::assertNoTaskPropertiesProblems);
     }
 
 }
